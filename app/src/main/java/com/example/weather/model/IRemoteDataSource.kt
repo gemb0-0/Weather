4c085b96365ba772db41b093c8f8847ab90c05a5
gemb0_0 <agamal00500@gmail.com>
2024-09-24 09:51:32 +0300

    assets

--- app/src/main/java/com/example/weather/model/IRemoteDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.weather.model
-
-import retrofit2.Response
-
-interface IRemoteDataSource {
-    suspend fun getWeather(lon: String, lat: String, appid: String,units:String,lang:String): Response<WeatherResponse>
-}
